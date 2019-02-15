115f5fa816acfc36586f1bc274d6037ff8b69c6b
roxanakoleva <roxana.koleva@abv.bg>
2019-02-15 20:04:24 +0200

    Add rating and pages in novel form.

--- src/main/java/com/kaufland/myLibrary/dto/BasicBookDTO.java
+++ src/main/java/com/kaufland/myLibrary/dto/BasicBookDTO.java
@@ -6,6 +6,33 @@ public class BasicBookDTO {
     private String title;
     private AuthorDTO author;
     private Instant year;
+    private String image;
+    private float rating;
+    private int pages;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
 
     public String getTitle() {
         return title;
